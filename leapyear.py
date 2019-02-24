328eb01fa71db219ad62b78aad4cc6382e838fc9
Akbar Ali <32708896+coderspot@users.noreply.github.com>
2019-02-24 21:36:31 +0530

    Update leapyear.py

--- leapyear.py
+++ leapyear.py
@@ -1,6 +1,17 @@
-year = int(input())
-
-if (( year%400 == 0)or (( year%4 == 0 ) and ( year%100 != 0))):
-    print("yes")
+print("Enter 'x' for exit.");
+num = input();
+if num == 'x':
+    exit();
+try:
+    year = int(num);
+except ValueError:
+    print("Please, enter year...exiting...");
 else:
-    print("no")
+    if((year%4 == 0) and (year%100 != 0)):
+        print("yes");
+    elif((year%100 == 0) and (year%400 == 0)):
+    	print("yes");
+    elif(year%400 == 0):
+    	print("yes");
+    else:
+    	print("no");
